a8b42267267c023133aaa9d6d6d43f31e26559d1
Gerardr92 <91161791+Gerardr92@users.noreply.github.com>
2021-10-26 23:53:33 -0600

    Add files via upload

--- BL.Restaurante/AgOrdenesBL.cs
+++ BL.Restaurante/AgOrdenesBL.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,45 +10,88 @@ namespace BL.Restaurante
 {
    public class AgOrdenesBL
     {
+        Contexto _contexto;
        public BindingList<Orden> ListaOrdenes { get; set; }
 
 
         public AgOrdenesBL()
         {
+            _contexto = new Restaurante.Contexto();
             ListaOrdenes = new BindingList<Orden>();
 
-            var Orden1 = new Orden();
-            Orden1.id = 001;
-            Orden1.descripcion = "Hamburguesa Clasica";
-            Orden1.precio = 120.00;
-            Orden1.existencia = 50;
-            Orden1.activo = true;
-
-            ListaOrdenes.Add(Orden1);
-
-            var Orden2 = new Orden();
-            Orden2.id = 002;
-            Orden2.descripcion = "Alitas BBQ";
-            Orden2.precio = 120.00;
-            Orden2.existencia = 100;
-            Orden2.activo = true;
-
-            ListaOrdenes.Add(Orden2);
-
-            var Orden3 = new Orden();
-            Orden3.id = 003;
-            Orden3.descripcion = "Arroz Frito ";
-            Orden3.precio = 140.00;
-            Orden3.existencia = 30;
-            Orden3.activo = true;
-
-            ListaOrdenes.Add(Orden3);
-
+            
         }
 
         public BindingList<Orden> ObtenerOrdenes()
         {
+            _contexto.Ordenes.Load();
+            ListaOrdenes = _contexto.Ordenes.Local.ToBindingList();
             return ListaOrdenes;
+        }
+
+        public Resultado GuardarOrden(Orden orden)
+        {
+
+            var resultado = Validar(orden);
+                if(resultado.Exitoso == false)
+            {
+                return resultado;
+            }
+            if (orden.id == 0)
+            {
+                orden.id = ListaOrdenes.Max(item => item.id) + 1;
+            }
+            resultado.Exitoso = true;
+            return resultado;
+        }
+
+        public void agregarOrden()
+        {
+            var nuevaOrden = new Orden();
+
+            ListaOrdenes.Add(nuevaOrden);
+        }
+        
+        public bool eliminarOrden(int id)
+        {
+            foreach (var item in ListaOrdenes)
+            {
+                if(item.id == id)
+                {
+                    ListaOrdenes.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        private Resultado Validar(Orden orden)
+        {
+            var Resultado = new Resultado();
+            Resultado.Exitoso = true;
+
+            if (string.IsNullOrEmpty(orden.descripcion) == true)
+            {
+                Resultado.Mensaje = "Ingrese una descripcion";
+                Resultado.Exitoso = false;
+            }
+
+            if (orden.existencia < 0)
+            {
+                Resultado.Mensaje = "La existencia debe ser mayor a cero";
+                Resultado.Exitoso = false;
+            }
+
+            if (orden.precio < 0)
+            {
+                Resultado.Mensaje = "El precio debe ser mayor a cero";
+                Resultado.Exitoso = false;
+            }
+
+
+            return Resultado;
+
         }
     }
 
@@ -58,5 +102,12 @@ namespace BL.Restaurante
         public double precio { get; set; }
         public int existencia { get; set; }
         public bool activo { get; set; }
+        public string observaciones { get; set; }
+    }
+
+    public class Resultado
+    {
+        public bool Exitoso { get; set; }
+        public string Mensaje { get; set; }
     }
 }
---
+++ BL.Restaurante/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Restaurante
+{
+   public class Contexto : DbContext
+    {
+        public Contexto(): base("Menu")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Orden> Ordenes { get; set; }
+    }
+}
